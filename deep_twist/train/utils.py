8ba6820580dcae4fc4f2be43dd4c5843037f218b
Chandler Watson <watsonc@stanford.edu>
2018-06-05 07:56:28 +0000

    Add base of training loop

--- deep_twist/train/utils.py
+++ deep_twist/train/utils.py
@@ -8,5 +8,16 @@ train_transform = torchvision.transforms.Compose([transforms.CenterCrop(351),
                                                   transforms.RandomTranslate(50),
                                                   transforms.Resize(227)])
 
-train_dataset = dataset.CornellGraspDataset(root_dir='cornell/', 
-                                            transform=train_transform)
+val_transform = torchvision.transforms.Compose(...)
+
+
+def train_model(train_data, dev_data, model, args):
+    train_data.transform = train_transform
+
+    optimizer = torch.optim.Adam(model.parameters() , lr=args.lr)
+    model.train()
+    for epoch in range(args.epochs):
+        # get train loss
+        pass
+        # get val loss
+        pass
